75ab0e3ed3ef3cc9055302d6689427d3f9c99726
vrMithun <mithuncsegvsraja@gmail.com>
2024-07-08 22:51:25 +0530

    myCode

---
+++ leetcode/2011FinalValue.java
@@ -0,0 +1,13 @@
+class solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("X++") || operations[i].equals("++X")) {
+                x++;
+            } else if (operations[i].equals("X--") || operations[i].equals("--X")) {
+                x--;
+            }
+        }
+        return x;
+    }
+}
